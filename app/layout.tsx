761f6a91452d8ad2ee758d15e79bcb8bc1de44fc
Konstantyn <72542265+KStrochan@users.noreply.github.com>
2026-07-21 18:57:05 +0300

    Fix components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,9 @@
-// src/app/layout.tsx
-// src/app/layout.tsx
-import type { Metadata } from 'next'; // Додай рядок з type
-import './globals.css';
-// ... інші імпорти
-import './globals.css';
+import type { Metadata } from 'next';
 import './globals.css';
 import Header from '@/components/Header/Header';
 import Footer from '@/components/Footer/Footer';
 import TanStackProvider from '@/components/TanStackProvider/TanStackProvider';
+
 export const metadata: Metadata = {
   title: 'NoteHub',
   description: 'Application designed for managing personal notes.',
